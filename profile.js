908a4c6a50689c7b5e70abff44ce277a5055218e
dyoungb612fox <dyoungb612@gmail.com>
2026-07-17 06:46:38 -0400

    Update student name in profile.js

--- profile.js
+++ profile.js
@@ -11,7 +11,7 @@ If you do not have a link yet, leave that section as an empty list: []
 */
 
 const studentProfile = {
-  name: "Diana X Murray",
+  name: "Diana XY Murray",
 
   tagline: "DREAM-High Scholar | Computational Biology Portfolio",
 
